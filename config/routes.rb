ea4e92c8fe2e2c3bfa490820325f4590fd726998
sharilyn <sharilyn.neidhardt@gmail.com>
2013-11-20 21:40:46 -0500

    made edits and updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ShirtsApp::Application.routes.draw do
   root "shirts#index"
   # these are special: :index, :new, :create, :update, :edit, :destroy, :show
-  resources :shirts, only: [:index, :new]
+  resources :shirts, only: [:index, :new, :create, :show, :edit, :update]
   get "search", to: "search#index"
 end
